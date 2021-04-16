50a1bd3dc2d5c04fe607d51ecdefb16539653e54
ruchabhat <77939005+ruchabhat@users.noreply.github.com>
2021-04-16 15:29:25 -0400

    Update preprocessor for Buckingham2007
    Original processor was using incorrect dataset (OneMinuteData). This processor cleans data from the correct CGM data and removes unreasonable glucose values and regions of zero variability

--- R/Buckingham2007/preprocessor.r
+++ R/Buckingham2007/preprocessor.r
@@ -1,6 +1,9 @@
-# This is the script for processing Buckingham2007 data into the common format. 
-# Author: David Buchanan
-# Date: January 31st, 2020, edited June 13th, 2020 by Elizabeth Chun
+# This is the script for processing Buckingham CGMS data into the common format. 
+# Author: Rucha Bhat, edited by Shaun Cass
+# Date: April 16, 2021
+
+library(tidyverse)
+library(magrittr)
 
 # This study downloads as a folder containing data tables and forms
 # First download the entire dataset. Do not rename the downloaded folder
@@ -14,45 +17,53 @@ dataset <- "Buckingham2007"
 # We will set the working directory here in the created folder
 setwd(dataset)
 
-# This is the relative path to the CGM files with original names
-original <- "DirecNetNavigatorPilotStudy"
-file.path <- paste(original, original, "OneMinuteData/", sep = "/")
+# This is the relative path to the Medtronic CGMS CGM file with original names
+file.path <- "DirecNetNavigatorPilotStudy/DataTables/tblFNavGlucose.csv"
+# Alternatively, if the file structure has been changed, simply place the CGM.txt file into the created folder
+# Then run the file path as follows:
+# file.path <- "tblCDataCGMS.csv"
 
-# This will list only the csv files of CGM data
-files <- dir(file.path, pattern = ".csv")
+# Read the raw data in 
+curr = read.csv(file.path, header = TRUE, stringsAsFactors = FALSE)
 
-# One can then loop through the files 
-nfiles = length(files)
+# combine date and time into standard format (POSIX1t format)
+curr$time = strptime(paste(as.Date(curr$NavReadDt), curr$NavReadTm),
+                     format = "%Y-%m-%d %H:%M:%S")
 
-for (i in 1:nfiles){
-  filename = files[i] # Get the file name
-  
-  # Read each csv in
-  curr = read.csv(paste(file.path, filename, sep = ""))
-  
-  # Getting the dates and times from the csv
-  dates = as.Date(curr$Date,"%m/%d/%Y")
-  times = strftime(strptime(curr$Time, "%I:%M %p"), format = "%H:%M:%S", tz="")
-  
-  # Creating the standard format "time" column
-  curr$"time" = as.POSIXct(paste(dates, times), format="%Y-%m-%d %H:%M:%S")
-  
-  # Reordering the dataframe to have only the columns we want in the order we want
-  curr = curr[c(1,22,5)]
-  
-  # Renaming the columns the standard format names
-  colnames(curr) = c("id","time","gl")
-  
-  #Ensure glucose values are recorded as numeric
-  curr$gl = as.numeric(curr$gl)
-  
-  # This dataset has some NA values for glucose readings, 
-  # If you would like to filter them out, simply uncomment this code:
-  # curr = na.omit(curr)
-  
-  # Write to the csv in the standard format
-  # Appending so the program only has to load one csv in at a time
-  write.table(curr, file = paste(dataset, "_processed.csv", sep = ""), 
-              row.names=F, col.names = !file.exists(paste(dataset, "_processed.csv", sep = "")),
-              append = T, sep = ",")
+# reorder and select only id, time, gl columns
+curr = curr[, c(2,6,5)]
+
+# Renaming the columns with the standard format names
+colnames(curr) = c("id","time","gl")
+
+#Ensure glucose values are recorded as numeric
+curr$gl = as.numeric(curr$gl)
+
+# Change all values less than 32 mg/dL and above 450 mg/dL to NA
+curr$gl[curr$gl <= 32] <- NA
+curr$gl[curr$gl > 450] <- NA
+
+# This dataset has some NA values for glucose readings, 
+# If you would like to filter them out, simply uncomment this code:
+# curr = na.omit(curr)
+
+# The following function is used to remove regions of zero variability
+# These regions likely arise from cgm sensor errors.
+
+zero.remove = function(tab){
+  tab %>%
+    mutate(diff = gl - lag(gl)) %>% 
+    filter(diff != 0, diff != lag(diff), diff != lag(diff, 2)) %>%
+    select(-diff)
 }
+
+curr = curr %>% group_split(id) %>% map_dfr(zero.remove)
+
+# Save the cleaned data to the created dataset folder
+# The cleaned file will be named "dataset"_processed.csv
+write.table(curr, file = paste(dataset, "_processed.csv", sep = ""), row.names = F, 
+            col.names = !file.exists(paste(dataset, "_processed.csv", sep = "")), 
+            sep = ",")
+
+# If instead you want to save the cleaned dataset as an .RData file uncomment:
+# save(curr, file = paste(dataset, "_processed.RData", sep = ""))
