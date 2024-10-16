bec4630b8d1e7903a6375016ad70b755ff05922d
Xinran Xu <70766915+Charlotte1031@users.noreply.github.com>
2024-10-16 10:11:10 -0400

    Update preprocessor.R

--- R/O_Mally2021/preprocessor.R
+++ R/O_Mally2021/preprocessor.R
@@ -1,72 +1,34 @@
+# This is the script for processing O_Mally2021 data into the common format. 
+# Author: Samuel Tan
+# Date: 10/10/2024
+
 library(tidyverse)
 library(haven)
 library(lubridate)
 
-
 cgmData <- read.table("cgm.txt", sep = "|", header = TRUE)
 screeningData <- read.table("DiabScreening_a.txt", sep = "|", header = TRUE, fill = TRUE, fileEncoding = "UTF-16LE", stringsAsFactors = FALSE, na.strings = "")
 
+# Merge the CGM data and screening data based on the "PtID" column
 merged_data <- cgmData %>%
   left_join(screeningData, by = "PtID")
 
+
+# Create a final dataset with new variables and selected columns
 final_data <- merged_data %>%
   mutate(
-    id = PtID + 5000,
-    time = dmy_hms(DataDtTm),
-    gl = as.numeric(CGM),
-    age = AgeAtEnrollment,
-    sex = Gender,
-    insulinModality = 1,
-    type = 1,
-    device = "Dexcom G6",
-    dataset = "o_mally2021"
-  ) %>%
+    id = PtID + 5000,              # Create a new 'id' by adding 5000 to the 'PtID' column
+    time = dmy_hms(DataDtTm),      # Convert the 'DataDtTm' column (date-time string) to a proper date-time format using 'lubridate'
+    gl = as.numeric(CGM),          # Convert the 'CGM' column to a numeric data type 
+    age = AgeAtEnrollment,         # Rename 'AgeAtEnrollment' to 'age'
+    sex = Gender,                  # Rename 'Gender' to 'sex' 
+    insulinModality = 1,           # Assign a constant value of 1 (as using Insulin Pump) 
+    type = 1,                      # Assign a constant value of 1 (as Type 1 Diabetic) 
+    device = "Dexcom G6",          # Assign the CGM device used ("Dexcom G6")
+    dataset = "o_mally2021"        # Label the dataset with the source or study name ('o_mally2021') 
+   ) %>%
   select(id, time, gl, age, sex, insulinModality, type, device, dataset)
 
 final_data %>% write_csv("csv_data/o_mally2021.csv")
 
 
-  # Summary statistics
-# individual_stats <- final_data %>%
-#   group_by(id) %>%
-#   summarise(
-#     Age = first(age),  # Assuming age is constant for each individual
-#     Sex = first(sex),  # Assuming sex is constant for each individual
-#     InsulinModality = first(insulinModality),  # Assuming insulin modality is constant for each individual
-#     Device = first(device),  # Assuming device is constant for each individual
-#     Duration_Weeks = as.numeric(difftime(max(time), min(time), units = "weeks")), # Duration in weeks for each individual
-#     gl_Mean = mean(gl, na.rm = TRUE), # Mean glycemic level
-#     gl_SD = sd(gl, na.rm = TRUE) # SD of glycemic level
-#   )
-# 
-# # Calculate summary statistics for the overall dataset
-# summary_stats <- individual_stats %>%
-#   summarise(
-#     N = n(),
-#     Age_Mean = mean(Age, na.rm = TRUE),
-#     Age_SD = sd(Age, na.rm = TRUE),
-#     Age_Min = min(Age, na.rm = TRUE),
-#     Age_Max = max(Age, na.rm = TRUE),
-#     Duration_Mean = mean(Duration_Weeks, na.rm = TRUE),
-#     Duration_SD = sd(Duration_Weeks, na.rm = TRUE),
-#     Duration_Min = min(Duration_Weeks, na.rm = TRUE),
-#     Duration_Max = max(Duration_Weeks, na.rm = TRUE),
-#     gl_Mean = mean(gl_Mean, na.rm = TRUE),
-#     gl_SD = mean(gl_SD, na.rm = TRUE),
-#     gl_Min = min(gl_Mean, na.rm = TRUE),
-#     gl_Max = max(gl_Mean, na.rm = TRUE),
-#     Male = sum(Sex == "M") / N * 100,
-#     Female = sum(Sex == "F") / N * 100,
-#     Pump = sum(InsulinModality == 1) / N * 100,  # Assuming 1 = Pump, adjust if necessary
-#     Injection = sum(InsulinModality == 0) / N * 100,  # Assuming 0 = Injection, adjust if necessary
-#     Dexcom = sum(Device == "Dexcom G6") / N * 100,
-#     Medtronic = sum(Device == "Medtronic") / N * 100
-#   )
-# 
-# gl_stats <- final_data %>%
-#   summarise(
-#     gl_Mean = mean(gl, na.rm = TRUE),
-#     gl_SD = sd(gl, na.rm = TRUE),
-#     gl_Min = min(gl, na.rm = TRUE),
-#     gl_Max = max(gl, na.rm = TRUE)
-#   )
