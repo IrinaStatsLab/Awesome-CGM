b30639959de3cc69c2cfecdf01a89be3794a486a
Xinran Xu <70766915+Charlotte1031@users.noreply.github.com>
2024-11-03 20:53:38 -0500

    fixing O_Malley

--- R/filter_missing_data.R
+++ R/filter_missing_data.R
@@ -13,19 +13,19 @@ process_dataset <- function(file_path) {
   # Convert 'time' column to datetime and extract date
   data$time <- as.POSIXct(data$time)
   data$date <- as.Date(data$time)
-  
+
   # Calculate time span for each subject
   subject_time_span <- data %>%
     group_by(id) %>%
     summarise(min_time = min(date, na.rm = T),
               max_time = max(date, na.rm = T)) %>%
     mutate(time_span_days = as.numeric(difftime(max_time, min_time, units = "days")) + 1)
-  
+
   # Calculate the number of distinct days with data entries for each subject
   distinct_days <- data %>%
     group_by(id) %>%
     summarise(distinct_days_with_data = n_distinct(date))
-  
+
   # Function to calculate inclusion based on your criteria
   calculate_inclusion <- function(time_span_days, distinct_days_with_data, total_entries, file_path) {
     if(file_path == "csv_data/buckingham2007.csv"){
@@ -60,34 +60,34 @@ process_dataset <- function(file_path) {
       }
     }
   }
-  
+
   # Count total entries per subject
   total_entries_per_subject <- data %>%
     group_by(id) %>%
     summarise(total_entries = n())
-  
+
   # Join all data into one table
   subject_metrics <- subject_time_span %>%
     left_join(distinct_days, by = "id") %>%
     left_join(total_entries_per_subject, by = "id")
-  
+
   # Apply the inclusion criteria to each subject
   subject_metrics <- subject_metrics %>%
-    mutate(inclusion = mapply(calculate_inclusion, 
-                              time_span_days, 
-                              distinct_days_with_data, 
+    mutate(inclusion = mapply(calculate_inclusion,
+                              time_span_days,
+                              distinct_days_with_data,
                               total_entries,
                               file_path),
            missingness_proportion = 1 - total_entries / (time_span_days * 144))
-  
+
   # List the subjects that need to be excluded (inclusion = 0) and their missingness proportion
   exclusion_ids <- subject_metrics %>%
     filter(inclusion == 1) %>%
     pull(id)
-  
+
   # Filter out ids that have too high of missingness
   filtered_data = data %>% filter(id %in% exclusion_ids)
-  
+
   # Add "_filtered.csv" to the file path for final data
   new_file_path <- paste0(file_path_sans_ext(file_path), "_filtered.csv")
   print(nrow(data))
@@ -110,4 +110,3 @@ process_all_datasets <- function(folder_path, output_file, output_vector) {
 
 # Run the function to process all datasets and save results to 'Exclusion_check.txt' and save removeable ids
 process_all_datasets("csv_data")
-
